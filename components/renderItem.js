fa9d2e3ca8c387fcac2ca70c4fd0c12002b8d6c9
Anzormumladze <46164149+Anzormumladze@users.noreply.github.com>
2019-10-01 14:38:53 +0400

    :100:

---
+++ components/renderItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+
+
+const renderItem = item => (
+    <View>
+
+    </View>
+)
+
+const styles = StyleSheet.create({
+
+
+
+})
+
+
+export default renderItem;
---
+++ components/searchContacts.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+
+
+const searchContacts = item => (
+    <View>
+
+    </View>
+)
+
+const styles = StyleSheet.create({
+
+
+
+})
+
+
+export default searchContacts;
